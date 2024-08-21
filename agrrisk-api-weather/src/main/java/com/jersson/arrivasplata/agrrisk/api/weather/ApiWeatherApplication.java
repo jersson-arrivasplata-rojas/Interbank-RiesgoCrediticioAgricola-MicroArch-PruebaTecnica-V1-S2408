0bc267ffa7c80623a049e7fd4ca2d85ade920645
Jersson Arrivasplata Rojas@ <developer@jerssonarrivasplata.com>
2024-08-21 07:30:41 -0500

    Add dependecys about logs

--- agrrisk-api-weather/src/main/java/com/jersson/arrivasplata/agrrisk/api/weather/ApiWeatherApplication.java
+++ agrrisk-api-weather/src/main/java/com/jersson/arrivasplata/agrrisk/api/weather/ApiWeatherApplication.java
@@ -3,7 +3,6 @@ package com.jersson.arrivasplata.agrrisk.api.weather;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ApiWeatherApplication {
 	
